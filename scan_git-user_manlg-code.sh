c36b21d5f726ad9c88d6d5ba2d3abcc8b9b93aae
manlg-code <luongtrieuman2004@gmail.com>
2025-10-31 13:16:29 +0700

    edit

--- scan_git-user_manlg-code.sh
+++ scan_git-user_manlg-code.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-curl https://github.com/manlg-code/ | grep class=\"repo\" | grep -Po "(?<=href=\").*?(?=\" )"
+repo=$(curl https://github.com/manlg-code/ | grep class=\"repo\" | grep -Po "(?<=href=\").*?(?=\" )")
+repo_count=$(echo "$repo" | wc -l)
+
+if [[ "$repo_count" = "0" || "$repo_count" = "1" ]] ; then 
+
+echo -e "\e[93m"$repo_count"\e[0m repo exist in manlg-code are in this list below:\n\e[93m$repo\e[0m" ; else
+
+echo -e "\e[93m"$repo_count"\e[0m repos exist in manlg-code are in this list below:\n\e[93m$repo\e[0m" ; fi
